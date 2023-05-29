d4f86f502894cb633f957944833c697d30123ebc
BOGGALA YASWANTH REDDY <93768814+yash2700@users.noreply.github.com>
2023-05-29 13:51:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+document.getElementById('Btn').addEventListener('click', ()=>{
+	document.getElementById('status').innerHTML='Entered Metaverse';
+	// document.querySelector('.entered').style.display='block';
+})
